769355b2153d8d9972c69df14dda318945112a98
Mohamed Taha <104592392+mopota@users.noreply.github.com>
2025-07-06 11:47:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-
 document.getElementById("mode-toggle").onclick = function() {
   document.body.classList.toggle("dark");
+  const icon = document.getElementById("mode-icon");
+  if(document.body.classList.contains("dark")) {
+    icon.src = "images/dark_mode_black.png";
+    icon.alt = "وضع داكن";
+  } else {
+    icon.src = "images/light_mode_white.png";
+    icon.alt = "وضع فاتح";
+  }
 };
 
 document.getElementById("search").onkeyup = function() {
